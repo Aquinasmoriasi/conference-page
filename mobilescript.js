21f4c5707de499f6d19b2e1a88977c91d7fcce87
unknown <aquinasmoriasi@gmail.com>
2022-06-30 23:41:49 +0300

    Add speakers dynamically

--- mobilescript.js
+++ mobilescript.js
@@ -15,7 +15,7 @@ ham.addEventListener('click', () => {
   }
 });
 
-menuItem.forEach((a) => {
+menuItem.forEach((link) => {
   if (ham.classList.contains('active')) {
     link.addEventListener('click', () => {
       ham.classList.remove('active');
@@ -29,27 +29,70 @@ menuItem.forEach((a) => {
   }
 });
 
-/*speakers section*/
-
+/* speakers section */
 
 const speakers = [
   {
-    name: `Greta Thurnberg`,
-    occupation: `World acclaimed environmental activist`,
-    image: `src="./images/greta.jpg/" alt="Greta Thurnberg"`,
-    ideoligies: `She is known for challenging world leaders to take immediate action for climate change mitigation.`
+    name: 'Greta Thurnberg',
+    picture: './images/greta.png',
+    occupation: 'World acclaimed environmental activist',
+    description: 'She is known for challenging world leaders to take immediate action for climate change mitigation.',
+  },
+  {
+    name: 'Melinda Gates',
+    picture: './images/gates.png',
+    occupation: 'Co-founder of the Bill and Melinda gates foundation',
+    description: 'She has been very instrumental in the fight against climate change using her influence and deep pockets.',
+  },
+  {
+    name: 'Mamo B. Mamo',
+    picture: './images/mamo.png',
+    occupation: 'CEO National Environmental Protection Agency Kenya',
+    description: 'She is known for challenging world leaders to take immediate action for climate change mitigation.',
+  },
+  {
+    name: 'Inger Andersen',
+    picture: './images/inger.png',
+    occupation: 'CEO, the United Nations Environmental Program',
+    description: 'She is also the founder and board member of Women in Science movement',
+  },
+  {
+    name: 'Njeri Kabeberi',
+    picture: './images/njeri.png',
+    occupation: 'Director, Green Peace Organization Kenya',
+    description: 'She holds a postgraduate doctorate in environmental impact assessment',
+  },
+  {
+    name: 'Awer Mohamed',
+    picture: './images/awer.png',
+    occupation: 'Director of the WWF foundation Kenya',
+    description: 'He is reknown for his tough stance on matters environment',
   }
 ];
 
+const section = document.createElement('section');
+section.setAttribute('id', 'speakers');
 const main = document.querySelector('main');
-let section = document.createElement('section');
-section.setAttribute('id', 'speakers')
-let h3 = document.querySelector('h3');
-h3.innerHTML = speaker[0].name;
-h3.setAttribute('class','name');
-section.append(h3);
-let p = document.createElement('p');
-p.innerHTML = speaker[0].occupation;
-p.setAttribute('class','occupation');
-section.append(p);
-main.append(section)
+
+for (let i in speakers) {
+const h3 = document.createElement('h3');
+h3.setAttribute('class', 'name');
+h3.innerHTML += speakers[i].name;
+const p = document.createElement('p');
+p.setAttribute('class', 'occupation');
+p.innerHTML += speakers[i].occupation;
+const img = document.createElement('img');
+img.setAttribute('src', speakers[i].picture);
+img.setAttribute('alt', speakers[i].picture);
+const p1 = document.createElement('p');
+p1.setAttribute('class', 'description');
+p1.innerHTML = speakers[i].description;
+const span = document.createElement('span');
+span.setAttribute('id', 'div1');
+
+const div = document.createElement('div');
+div.setAttribute('class', 'speaker-details');
+div.append(h3, p, span, p1);
+section.append(img, div);
+main.append(section);
+}
