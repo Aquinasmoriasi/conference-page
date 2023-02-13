ec26529371c0c8ce47b95501180847ac1ea40329
unknown <aquinasmoriasi@gmail.com>
2023-02-13 09:21:08 +0300

    Add speakers array as a module

---
+++ speakers/speakers.js
@@ -0,0 +1,45 @@
+const speakers = [
+  {
+    name: 'Greta Thurnberg',
+    picture: '../images/greta.png',
+    occupation: 'World acclaimed environmental activist',
+    description:
+      'She is known for challenging world leaders to take immediate action for climate change mitigation.',
+  },
+  {
+    name: 'Melinda Gates',
+    picture: '../images/gates.png',
+    occupation: 'Co-founder of the Bill and Melinda gates foundation',
+    description:
+      'She has been very instrumental in the fight against climate change using her influence and deep pockets.',
+  },
+  {
+    name: 'Mamo B. Mamo',
+    picture: '../images/mamo.png',
+    occupation: 'CEO National Environmental Protection Agency Kenya',
+    description:
+      'She is known for challenging world leaders to take immediate action for climate change mitigation.',
+  },
+  {
+    name: 'Inger Andersen',
+    picture: '../images/inger.png',
+    occupation: 'CEO, the United Nations Environmental Program',
+    description:
+      'She is also the founder and board member of Women in Science movement',
+  },
+  {
+    name: 'Njeri Kabeberi',
+    picture: '../images/njeri.png',
+    occupation: 'Director, Green Peace Organization Kenya',
+    description:
+      'She holds a postgraduate doctorate in environmental impact assessment',
+  },
+  {
+    name: 'Awer Mohamed',
+    picture: '../images/awer.png',
+    occupation: 'Director of the WWF foundation Kenya',
+    description: 'He is reknown for his tough stance on matters environment',
+  },
+];
+
+export default { speakers };
